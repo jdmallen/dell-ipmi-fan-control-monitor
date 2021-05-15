ef0fa2e23ab81d00e38492f9e60d249dc0e5b08c
J.D. Mallen <lightandtheheat@gmail.com>
2021-05-15 05:24:24 -0400

    Update to .NET 5, improve logs, upd pub profiles

--- JDMallen.IPMITempMonitor/Worker.cs
+++ JDMallen.IPMITempMonitor/Worker.cs
@@ -6,13 +6,15 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using JDMallen.Toolbox.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace JDMallen.IPMITempMonitor
 {
-	public class Worker : BackgroundService
+	public class Worker : ScopedBackgroundService<Worker>
 	{
 		private readonly ILogger<Worker> _logger;
 		private readonly Settings _settings;
@@ -22,6 +24,7 @@ namespace JDMallen.IPMITempMonitor
 		private bool _belowTemp;
 		private int _manualSwitchAttemptCount;
 		private readonly List<int> _lastTenTemps;
+		private int _lastRecordedTemp;
 
 		private const string CHECK_TEMPERATURE_CONTROL_COMMAND = "sdr type temperature";
 		private const string ENABLE_AUTOMATIC_TEMP_CONTROL_COMMAND = "raw 0x30 0x30 0x01 0x01";
@@ -31,7 +34,8 @@ namespace JDMallen.IPMITempMonitor
 		public Worker(
 			ILogger<Worker> logger,
 			IOptions<Settings> settings,
-			IHostEnvironment environment)
+			IHostEnvironment environment,
+			IServiceScopeFactory scopeFactory) : base(logger, scopeFactory)
 		{
 			_logger = logger;
 			_environment = environment;
@@ -39,75 +43,65 @@ namespace JDMallen.IPMITempMonitor
 			_lastTenTemps = new List<int>(_settings.RollingAverageNumberOfTemps);
 		}
 
-		protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+		private string _logPrefix
+			= "Current temp: {lastRecordedTemp}°C | Average temp: {rollingAverageTemp}°C | ";
+
+		protected override async Task ExecuteInScopeAsync(
+			IServiceScope scope,
+			CancellationToken stoppingToken)
 		{
-			_logger.LogInformation($"Detected OS: {_settings.Platform:G}.");
+			await CheckLatestTemperature(stoppingToken);
+			double rollingAverageTemp = GetRollingAverageTemperature();
 
-			while (!cancellationToken.IsCancellationRequested)
+			_logger.LogInformation(
+				_logPrefix + "Fan control: {operatingMode}",
+				_lastRecordedTemp,
+				rollingAverageTemp,
+				_currentMode);
+
+			// If the temp goes above the max threshold, immediately switch to AUTOMATIC fan mode.
+			if (_lastRecordedTemp > _settings.MaxTempInC || rollingAverageTemp > _settings.MaxTempInC)
 			{
-				int temp = await CheckLatestTemperature(cancellationToken);
-				PushTemperature(temp);
-				double rollingAverageTemp = GetRollingAverageTemperature();
-
-				_logger.LogInformation(
-					"{operatingMode} fan control; current temp {temp} C; average temp {rollingAverageTemp} C",
-					_currentMode,
-					temp,
-					rollingAverageTemp,
-					DateTimeOffset.Now);
-
-				// If the temp goes above the max threshold,
-				// immediately switch to Automatic fan mode.
-				if (temp > _settings.MaxTempInC || rollingAverageTemp > _settings.MaxTempInC)
+				_belowTemp = false;
+				if (_currentMode == OperatingMode.AUTOMATIC)
 				{
-					_belowTemp = false;
-					if (_currentMode == OperatingMode.Automatic)
-					{
-						await Delay(cancellationToken);
-
-						continue;
-					}
-
-					await SwitchToAutomaticTempControl(cancellationToken);
+					return;
 				}
 
-				// Only switch back to manual if both the current temp
-				// AND the rolling average are back below the set max.
-				else
-				{
-					if (!_belowTemp)
-					{
-						// Record the first record of when the temp dipped
-						// below the max temp threshold. This is an extra
-						// safety measure to ensure that Automatic mode isn't
-						// turned off too soon. You can see its usage in
-						// SwitchToManualTempControl().
-						_timeFellBelowTemp = DateTime.UtcNow;
+				await SwitchToAutomaticTempControl(stoppingToken);
 
-						// Reset the number of times we attempt to switch to
-						// Manual, based on setting (default: 2).
-						// Note that if the temperature goes above threshold
-						// between attempts, subsequent attempts are skipped
-						// and it switches to Automatic fan control mode
-						// (as one would hope).
-						_manualSwitchAttemptCount = _settings.ManualModeSwitchReattempts;
-					}
-
-					_belowTemp = true;
-
-					if (_currentMode == OperatingMode.Manual && _manualSwitchAttemptCount == 0)
-					{
-						await Delay(cancellationToken);
-
-						continue;
-					}
-
-					await SwitchToManualTempControl(cancellationToken);
-				}
-
-				await Delay(cancellationToken);
+				return;
 			}
+
+			// Only switch back to manual if both the current temp AND the rolling average are back
+			// below the set max.
+
+			if (!_belowTemp)
+			{
+				// Record the first record of when the temp dipped below the max temp threshold.
+				// This is an extra safety measure to ensure that AUTOMATIC mode isn't turned off
+				// too soon. You can see its usage in SwitchToManualTempControl.
+				_timeFellBelowTemp = DateTime.UtcNow;
+
+				// Reset the number of times we attempt to switch to MANUAL, based on setting
+				// (default: 2). Note that if the temperature goes above threshold between attempts,
+				// subsequent attempts are skipped and it switches to AUTOMATIC fan control mode
+				// (as one would hope).
+				_manualSwitchAttemptCount = _settings.ManualModeSwitchReattempts;
+			}
+
+			_belowTemp = true;
+
+			if (_currentMode == OperatingMode.MANUAL && _manualSwitchAttemptCount == 0)
+			{
+				return;
+			}
+
+			await SwitchToManualTempControl(stoppingToken);
 		}
+
+		protected override TimeSpan LoopDelay
+			=> TimeSpan.FromSeconds(_settings.PollingIntervalInSeconds);
 
 		private void PushTemperature(int temp)
 		{
@@ -120,13 +114,6 @@ namespace JDMallen.IPMITempMonitor
 		}
 
 		private double GetRollingAverageTemperature() => Math.Round(_lastTenTemps.Average(), 1);
-
-		private async Task Delay(CancellationToken cancellationToken)
-		{
-			await Task.Delay(
-				TimeSpan.FromSeconds(_settings.PollingIntervalInSeconds),
-				cancellationToken);
-		}
 
 		/// <summary>
 		/// Calls iDRAC for latest temperature.
@@ -143,17 +130,15 @@ namespace JDMallen.IPMITempMonitor
 		///     Temp             | 0Fh | ok  |  3.2 | 31 degrees C
 		/// </remarks>
 		/// <returns></returns>
-		private async Task<int> CheckLatestTemperature(CancellationToken cancellationToken)
+		private async Task CheckLatestTemperature(CancellationToken cancellationToken)
 		{
 			// Get the output string like the one in <remarks> above.
-			string result =
-				await ExecuteIpmiToolCommand(
-					CHECK_TEMPERATURE_CONTROL_COMMAND,
-					cancellationToken);
+			string result = await ExecuteIpmiToolCommand(
+				CHECK_TEMPERATURE_CONTROL_COMMAND,
+				cancellationToken);
 
-			// Using the default of (?<=0Eh|0Fh).+(\\d{2}) will return
-			// all 2-digit numbers in lines containing "0Eh" or "0Fh"--
-			// in this case, 30 and 31-- as captured groups.
+			// Using the default of (?<=0Eh|0Fh).+(\\d{2}) will return all 2-digit numbers in lines
+			// containing "0Eh" or "0Fh"-- in the above example, 30 and 31-- as captured groups.
 			var matches = Regex.Matches(
 				result,
 				_settings.RegexToRetrieveTemp,
@@ -162,64 +147,77 @@ namespace JDMallen.IPMITempMonitor
 			// For each matched line, grab the last capture group (the 2-digit
 			// temp) and attempt to convert it to an integer. Find the max
 			// int of all the matched lines and return it.
-			int maxCpuTemp = matches
-				.Select(
-					x => int.TryParse(
-						x.Groups.Values.LastOrDefault()?.Value,
-						out int temp)
+			int maxCpuTemp = matches.Select(
+					x => int.TryParse(x.Groups.Values.LastOrDefault()?.Value, out int temp)
 						? temp
 						: 0)
 				.Max();
 
-			return maxCpuTemp;
+			PushTemperature(maxCpuTemp);
+
+			_lastRecordedTemp = maxCpuTemp;
 		}
 
-		private async Task SwitchToAutomaticTempControl(CancellationToken cancellationToken)
+		private async Task SwitchToAutomaticTempControl(CancellationToken stoppingToken)
 		{
-			_logger.LogInformation("Switching to Automatic mode.");
+			double rollingAverageTemp = GetRollingAverageTemperature();
+			_logger.LogWarning(
+				_logPrefix + "Switching to {newOperatingMode} fan control",
+				_lastRecordedTemp,
+				rollingAverageTemp,
+				OperatingMode.AUTOMATIC);
 
 			await ExecuteIpmiToolCommand(
 				ENABLE_AUTOMATIC_TEMP_CONTROL_COMMAND,
-				cancellationToken);
+				stoppingToken);
 
-			_currentMode = OperatingMode.Automatic;
+			_currentMode = OperatingMode.AUTOMATIC;
 		}
 
-		private async Task SwitchToManualTempControl(CancellationToken cancellationToken)
+		private async Task SwitchToManualTempControl(CancellationToken stoppingToken)
 		{
-			var timeSinceLastActivation =
-				DateTime.UtcNow - _timeFellBelowTemp;
+			var timeSinceLastActivation = DateTime.UtcNow - _timeFellBelowTemp;
 
-			var threshold =
-				TimeSpan.FromSeconds(_settings.BackToManualThresholdInSeconds);
+			var threshold = TimeSpan.FromSeconds(_settings.BackToManualThresholdInSeconds);
 
 			if (timeSinceLastActivation < threshold)
 			{
 				var secondsRemaining =
 					(int) (threshold - timeSinceLastActivation).TotalSeconds;
-				_logger.LogWarning(
-					"Manual threshold not yet crossed. Staying in Automatic mode for at least another {remaining} {unit}.",
+				_logger.LogInformation(
+					_logPrefix
+					+ "{newOperatingMode} delay threshold not yet met; "
+					+ "staying in {operatingMode} mode for {remaining} {unit}.",
+					_lastRecordedTemp,
+					GetRollingAverageTemperature(),
+					OperatingMode.MANUAL,
+					OperatingMode.AUTOMATIC,
 					secondsRemaining,
 					secondsRemaining == 1 ? "second" : "seconds");
 
 				return;
 			}
 
-			_logger.LogInformation(
-				"Switching to Manual mode, attempt {attemptNumber} of {totalAttemptCount}",
+			_logger.LogWarning(
+				_logPrefix
+				+ "Switching to {newOperatingMode} fan control | "
+				+ "Attempt {attemptNumber} of {totalAttemptCount}",
+				_lastRecordedTemp,
+				GetRollingAverageTemperature(),
+				OperatingMode.MANUAL,
 				_settings.ManualModeSwitchReattempts - _manualSwitchAttemptCount + 1,
 				_settings.ManualModeSwitchReattempts);
 
 			await ExecuteIpmiToolCommand(
 				DISABLE_AUTOMATIC_TEMP_CONTROL_COMMAND,
-				cancellationToken);
+				stoppingToken);
 
 			string fanSpeedCommand = string.Format(
 				STATIC_FAN_SPEED_FORMAT_STRING,
 				_settings.ManualModeFanPercentage.ToString("X"));
 
-			await ExecuteIpmiToolCommand(fanSpeedCommand, cancellationToken);
-			_currentMode = OperatingMode.Manual;
+			await ExecuteIpmiToolCommand(fanSpeedCommand, stoppingToken);
+			_currentMode = OperatingMode.MANUAL;
 			if (_manualSwitchAttemptCount >= 1)
 			{
 				_manualSwitchAttemptCount--;
@@ -228,7 +226,7 @@ namespace JDMallen.IPMITempMonitor
 
 		private async Task<string> ExecuteIpmiToolCommand(
 			string command,
-			CancellationToken cancellationToken)
+			CancellationToken stoppingToken)
 		{
 			// Uses default path for either Linux or Windows,
 			// unless a path is explicitly provided in appsettings.json.
@@ -242,39 +240,32 @@ namespace JDMallen.IPMITempMonitor
 					}
 					: _settings.PathToIpmiToolIfNotDefault;
 
-			var args =
-				$"-I lanplus -H {_settings.IpmiHost} -U {_settings.IpmiUser} -P {_settings.IpmiPassword} {command}";
+			string args =
+				$"-I lanplus -H {_settings.IpmiHost} -U {_settings.IpmiUser} "
+				+ $"-P {_settings.IpmiPassword} {command}";
 
 			_logger.LogDebug(
-				$"Executing: {ipmiPath} {args.Replace(_settings.IpmiPassword, "{password}")}");
+				"Executing: {ipmiPath} {args}",
+				ipmiPath,
+				args.Replace(_settings.IpmiPassword, "{password}"));
 
 			if (_environment.IsDevelopment())
 			{
 				return command switch
 				{
-					CHECK_TEMPERATURE_CONTROL_COMMAND =>
-
-						// Your IPMI results may differ from my sample.
-
-						await File.ReadAllTextAsync(
-							Path.Combine(
-								AppContext.BaseDirectory,
-								$"..{Path.DirectorySeparatorChar}",
-								$"..{Path.DirectorySeparatorChar}",
-								$"..{Path.DirectorySeparatorChar}",
-								"test_temp_response.txt"),
-							cancellationToken),
-					_ => string.Empty,
+					// Your IPMI results may differ from my sample.
+					CHECK_TEMPERATURE_CONTROL_COMMAND => await ReadTestResponseFile(stoppingToken),
+					_                                 => string.Empty,
 				};
 			}
 
-			return await RunProcess(ipmiPath, args, cancellationToken);
+			return await RunProcess(ipmiPath, args, stoppingToken);
 		}
 
 		private async Task<string> RunProcess(
 			string path,
 			string args,
-			CancellationToken cancellationToken)
+			CancellationToken stoppingToken)
 		{
 			string result = null;
 			var process = new Process
@@ -293,48 +284,85 @@ namespace JDMallen.IPMITempMonitor
 			try
 			{
 				process.Start();
-				process.WaitForExit();
+				await process.WaitForExitAsync(stoppingToken);
 				result = await process.StandardOutput.ReadToEndAsync();
 			}
 			catch (Exception ex)
 			{
 				_logger.LogCritical(ex, "Error attempting to call ipmitool!");
-				await StopAsync(cancellationToken);
+				await StopAsync(stoppingToken);
 			}
 
 			return result;
 		}
 
+		private async Task<string> ReadTestResponseFile(CancellationToken stoppingToken)
+		{
+			try
+			{
+				return await File.ReadAllTextAsync(
+					Path.Combine(
+						AppContext.BaseDirectory,
+						$"..{Path.DirectorySeparatorChar}",
+						$"..{Path.DirectorySeparatorChar}",
+						$"..{Path.DirectorySeparatorChar}",
+						"test_temp_response.txt"),
+					stoppingToken);
+			}
+			catch (FileNotFoundException ex)
+			{
+				_logger.LogWarning(ex, "Unable to find test file; returning empty string.");
+
+				return string.Empty;
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, "Unknown error reading test file; returning empty string.");
+
+				return string.Empty;
+			}
+		}
+
 		/// <summary>
 		/// Triggered when the application host is ready to start the service.
 		/// </summary>
-		/// <param name="cancellationToken">Indicates that the start process has been aborted.</param>
-		public override async Task StartAsync(CancellationToken cancellationToken)
+		/// <param name="stoppingToken">
+		/// Indicates that the start process has been aborted.
+		/// </param>
+		public override async Task StartAsync(CancellationToken stoppingToken)
 		{
-			_logger.LogInformation("Monitor starting. Setting fan control to automatic to start.");
+			_logger.LogDebug($"Detected OS: {_settings.Platform:G}.");
 
-			_currentMode = OperatingMode.Automatic;
+			await CheckLatestTemperature(stoppingToken);
 
-			await SwitchToAutomaticTempControl(cancellationToken);
+			_logger.LogInformation(
+				_logPrefix + "Monitor starting | Setting initial fan control to {operatingMode}",
+				_lastRecordedTemp,
+				GetRollingAverageTemperature(),
+				OperatingMode.AUTOMATIC);
 
-			await base.StartAsync(cancellationToken);
+			_currentMode = OperatingMode.AUTOMATIC;
+
+			await SwitchToAutomaticTempControl(stoppingToken);
+
+			await base.StartAsync(stoppingToken);
 		}
 
 		/// <summary>
 		/// Triggered when the application host is performing a graceful shutdown.
 		/// </summary>
-		/// <param name="cancellationToken">Indicates that the shutdown process should no longer be graceful.</param>
-		public override Task StopAsync(CancellationToken cancellationToken)
+		/// <param name="stoppingToken">Indicates that the shutdown process should no longer be graceful.</param>
+		public override Task StopAsync(CancellationToken stoppingToken)
 		{
 			_logger.LogWarning("Monitor stopping");
 
-			return base.StopAsync(cancellationToken);
+			return base.StopAsync(stoppingToken);
 		}
 	}
 
 	public enum OperatingMode
 	{
-		Automatic,
-		Manual,
+		AUTOMATIC,
+		MANUAL,
 	}
 }
